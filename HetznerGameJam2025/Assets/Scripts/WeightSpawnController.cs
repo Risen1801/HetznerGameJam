e5031f56ea2c58e92b00fc8d4477979d17c7bcc7
MafackerJones <lust20035@hs-ansbach.de>
2025-05-16 19:54:56 +0200

    First Prototyping of Idea 1 of Theme "Balance"
    - Idea: Weights fall onto the Scale and get combined into heavier weights
    
    - Added own Scene and Script to move Weight until its dropped

---
+++ HetznerGameJam2025/Assets/Scripts/WeightSpawnController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class WeightSpawnController : MonoBehaviour
+{
+    public GameObject[] Weights;
+    public float Speed = 10;
+    public float minRangeX = -4.5f;
+    public float maxRangeX = -1.5f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.Translate(Vector3.right * inputHorizontal * Speed * Time.deltaTime);
+        
+
+        float clampedX = Mathf.Clamp(transform.position.x, minRangeX, maxRangeX);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+
+}
